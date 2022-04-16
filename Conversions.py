40d54937ed0f84bde5eedfc59d8489414a71d47f
Faisal-Sey <phaisalsey6@gmail.com>
2022-04-16 16:17:27 +0000

    updated

--- Conversions.py
+++ Conversions.py
@@ -26,29 +26,36 @@ def current_dim_convert(dim):
 
 def getab(drop):
     current_dim, current_var = current_dim_convert(drop.get())
-    previous = ttk.Combobox(app, textvariable=drop2, values=current_var, font=myFont, width=28)
-    previous.place(x=80, y=200)
+    drop2.set("Select unit to convert from")
+    drop3.set("Select unit to convert to")
+    factor_text = ""
+    previous = ttk.Combobox(app, textvariable=drop2, values=current_var, font=myFont, width=40)
+    previous.place(anchor="c", relx=0.35, rely=0.25)
     fv = tk.DoubleVar()
-    from_value = Entry(app, font=myFont, textvariable=fv)
-    from_value.place(x=150, y=300)
-    next = ttk.Combobox(app, textvariable=drop3, values=current_var, font=myFont, width=25)
-    next.place(x=500, y=200)
+    from_value = Entry(app, font=myFont, textvariable=fv, width=40)
+    from_value.place(anchor="c", relx=0.33, rely=0.38)
+    next = ttk.Combobox(app, textvariable=drop3, values=current_var, font=myFont, width=40)
+    next.place(anchor="c", relx=0.7, rely=0.25)
     tv = tk.DoubleVar()
-    to_value = Entry(app, font=myFont, textvariable=tv, width=25)
-    to_value.place(x=560, y=300)
+    to_value = Entry(app, font=myFont, textvariable=tv, width=40)
+    to_value.place(anchor="c", relx=0.7, rely=0.38)
     label1 = Label(app, text="Input: ", font=myFont)
-    label1.place(x=50, y=300)
+    label1.place(anchor="c", relx=0.29, rely=0.34)
     label2 = Label(app, text="Output: ", font=myFont)
-    label2.place(x=450, y=300)
+    label2.place(anchor="c", relx=0.68, rely=0.34)
+
+    factor_label.place(anchor="c", relx=0.5, rely=0.45)
 
     def calculate_current_dim():
         converted_result = converts(current_dim, previous.get(), next.get())
         result = float(from_value.get()) * converted_result
         to_value.delete(0, END)
         to_value.insert(0, result)
+        factor_label["text"] = ""
+        factor_label["text"] = "multiplied by: " + str(converted_result)
 
     calculate_button = Button(app, fg='white', bg='blue', command=calculate_current_dim, text="Calculate", font=myFont)
-    calculate_button.place(x=400, y=400)
+    calculate_button.place(anchor="c", relx=0.5, rely=0.53)
 
 top_label = Label(app, text="CONVERSIONS FROM ONE UNIT TO ANOTHER", fg="blue")
 top_label.pack(padx=10, pady=10)
@@ -62,8 +69,9 @@ values=["Length", "Area", "Volume", "Mass", "Temperature", "Force", "Pressure", 
         "Angular Velocity", "Acceleration", "Moment of Inertia", "Kinematic Viscosity", "Fuel Efficiency to Mass", "Fuel Efficiency to Volume", "Specific Volume Converter",
         "Flow rate", "Electrical resistance","Dynamic viscosity", "Mass Flow rate", "Molar Flow rate", "Molarity", "Molality", "Surface Tension",
         "Specific Heat Capacity", "Heat Transfer Coefficient"]
-categories = ttk.Combobox(app, textvariable=drop, values=values, font = myFont, justify=tk.CENTER)
+categories = ttk.Combobox(app, textvariable=drop, values=values, font = myFont, justify=tk.CENTER, width=40)
 categories.pack(padx=10, pady=30)
+factor_label = Label(app, text="", font=myFont)
 drop2 = tk.StringVar()
 drop3 = tk.StringVar()
 drop2.set("Select unit to convert from")
